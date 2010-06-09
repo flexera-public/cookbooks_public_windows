957d14c7cc86b9dcd10cf6ffc21af531482ecc85
Jon Seaberg <jon@rightscale.com>
2010-06-09 14:40:25 -0700

    bump win_admin version

--- cookbooks/win_admin/metadata.rb
+++ cookbooks/win_admin/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "alex@rightscale.com"
 license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
 description      "Windows Admin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.3.1"
+version          "0.3.2"
 
 recipe "win_admin::default", "Not yet implemented"
 recipe "win_admin::change_admin_password", "Changes the administrator password"
