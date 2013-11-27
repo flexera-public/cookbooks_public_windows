37bd9c0ce08f7e782ec0fdc6892be6b3af6d0b2a
Dean Onishi <dean@rightscale.com>
2013-11-27 13:52:24 -0800

    Updated license code

--- cookbooks/blog_engine/metadata.rb
+++ cookbooks/blog_engine/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
-license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
+license          "Copyright RightScale, Inc. All rights reserved."
 description      "Install and configure the BlogEngine application, see http://www.dotnetblogengine.net"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.3.18"
